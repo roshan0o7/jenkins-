60caf3aba342b3c9d24452290b81881773c60eab
roshan0o7 <123960047+roshan0o7@users.noreply.github.com>
2023-12-27 18:03:51 +0530

    Create Dockerdemoapp.java

---
+++ Dockerdemoapp.java
@@ -0,0 +1,13 @@
+package com.jhooq.demo.Roshandockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoshanDockerDemoApplication {
+
+        public static void main(String[] args) {
+                SpringApplication.run(RoshanDockerDemoApplication.class, args);
+        }
+
+}
